eb5eb62f9c30e4f014637516345f2dc058c59c57
Dilmurod03 <dahmadjonov1031@gmail.com>
2022-05-26 17:11:55 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import render from './render.js'
+import newObject from './newObject.js'
+
+let form = document.getElementById('form')
+let ism = document.getElementById('userIsm')
+let num = document.getElementById('classNum')
+let gender = document.getElementById('userGender')
